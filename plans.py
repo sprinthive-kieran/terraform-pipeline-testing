aedac6aa9b4723a774da787112d2fdd4119f229f
sprinthive-kieran <133221787+sprinthive-kieran@users.noreply.github.com>
2024-04-15 13:50:17 +0200

    Update plans.py

--- plans.py
+++ plans.py
@@ -1,7 +1,14 @@
 import os
 import json
+import subprocess
 
 
 print(os.environ['envs'])
 for i in json.loads(os.environ['envs']):
-    print(i["name"])
+    currentEnv = i["name"]
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    print(currentEnv)
+    os.chdir(f"envs/{currentEnv}")
+    subprocess.call(["terraform", "init"])
+    subprocess.call(["terraform", 'plan', '-out', f'../../plans/{currentEnv}.plan'])
+
