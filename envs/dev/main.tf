b797b7f72590ddc2643312c1e9d64ddd739eace2
Kieran Roberts <kieran.roberts@sprinthive.com>
2024-04-15 17:07:36 +0200

    dump

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,9 +2,3 @@ resource "local_file" "foo" {
   content  = "Time is: ${plantimestamp()} for dev"
   filename = "${path.module}/time"
 }
-
-terraform {
-  backend "local" {
-    path = "terraform.tfstate"
-  }
-}
--- envs/staging/main.tf
+++ envs/staging/main.tf
@@ -2,9 +2,3 @@ resource "local_file" "foo" {
   content  = "Time is: ${plantimestamp()} for staging"
   filename = "${path.module}/time"
 }
-
-terraform {
-  backend "local" {
-    path = "terraform.tfstate"
-  }
-}
