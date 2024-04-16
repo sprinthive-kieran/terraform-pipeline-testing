ed5c4605a00d83ef75e0f3d7551f2b82e17afae3
Kieran Roberts <kieran.roberts@sprinthive.com>
2024-04-16 17:20:44 +0200

    cleanup

--- vars/terraformPlan.groovy
+++ vars/terraformPlan.groovy
@@ -5,27 +5,20 @@
  * @return
  */
 def call(config) {
-        stage('Check Plans') {
-            steps {
-                script {
-                      for (env in config.environments) {
-                        stage("Terraform Plan: $env.name") {
-                             sh "pwd"
-                              echo "$env.name"
-                              dir("envs/$env.name/") {
-                                sh "terraform init"
-                                sh "pwd"
-                                  if (env.workspace != "default") {
-                                      sh "terraform workspace select $env.workspace"
-                                  }
-
-                                sh "terraform plan -out ../../plans/$env.name-plan"
-                              }
-
-                        }
-                      }
-                }
-            }
-        }
-
+  stage('Check Plans') {
+    for (env in envsJson) {
+      stage("Terraform Plan: $env.name") {
+        sh "pwd"
+         echo "$env.name"
+         dir("envs/$env.name/") {
+           sh "terraform init"
+           sh "pwd"
+           if (env.workspace != "default") {
+               sh "terraform workspace select $env.workspace"
+           }
+           sh "terraform plan -out ../../plans/$env.name-plan"
+         }
+      }
+    }
+  }
 }
